db0ac56a262a6a9c742e1c6772972ec027c80883
rel0ker <rel0ker@ya.ru>
2024-11-14 21:27:56 +0300

    14.11

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import UserForm from './components/User-form.vue'
 </script>
 
 <template>
+  <UserForm></UserForm>
   <RouterView />
 </template>
 
---
+++ src/components/User-form.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Форма обратной связи</h1>
+    <form @submit.prevent="submitform">
+      <div>
+        <label for="name">Имя</label>
+        <input type="text" v-model="user.name" required />
+      </div>
+      <div>
+        <label for="email">почта</label>
+        <input type="email" v-model="user.email" required />
+      </div>
+      <div>
+        <label for="age">возраст</label>
+        <input type="number" v-model="user.age" required />
+      </div>
+      <button type="submit">Подтвердить</button>
+    </form>
+    <div v-if="submitted">
+      <h2>Полученная информация:</h2>
+      <p>Имя: {{ user.name }}</p>
+      <p>Почта: {{ user.email }}</p>
+      <p>Возраст: {{ user.age }}</p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  name: 'User-form',
+  setup() {
+    const user = ref({
+      name: '',
+      email: '',
+      age: null as number | null,
+    })
+    const submitted = ref(false)
+    const submitform = () => {
+      submitted.value = true
+    }
+    return {
+      user,
+      submitted,
+      submitform,
+    }
+  },
+})
+</script>
